d4b8410bc1bafdee49e3c7bda6b56a72d22f48df
Engin Niyazi Ergül Education <enginniyazi@gmail.com>
2025-08-11 01:03:09 +0300

    update

--- src/dist/src/models/categoryModel.js
+++
@@ -1,8 +0,0 @@
-// src/models/categoryModel.ts
-import { model, Schema } from 'mongoose';
-const CategorySchema = new Schema({
-    name: { type: String, required: true, unique: true, trim: true },
-    description: { type: String },
-}, { timestamps: true });
-const Category = model('Category', CategorySchema);
-export default Category;
--- src/dist/src/types/index.js
+++
@@ -1,9 +0,0 @@
-// src/types/index.ts
-export * from './userTypes.js';
-export * from './courseTypes.js';
-export * from './categoryTypes.js';
-export * from './instructorTypes.js';
-export * from './leadTypes.js';
-export * from './applicationTypes.js';
-export * from './couponTypes.js';
-export * from './campaignTypes.js';
